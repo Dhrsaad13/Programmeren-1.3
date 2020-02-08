2460b515bf8da384c668704aba4c1d7b4b11fdd8
Menno Kreukniet <m.kreukniet@student.avans.nl>
2020-02-08 15:22:27 +0100

    comit

--- Sa'ad Abdullah 2154528/app/src/main/java/com/example/android/sierendeelementen/ArtItem.java
+++ Sa'ad Abdullah 2154528/app/src/main/java/com/example/android/sierendeelementen/ArtItem.java
@@ -1,4 +1,61 @@
 package com.example.android.sierendeelementen;
 
 public class ArtItem {
+    private int objectID;
+    private String identificationNumber;
+    private String title;
+    private String artist;
+    private String material;
+    private String description;
+    private String surface;
+    private String dateOfPlacement;
+    private String img;
+
+    public ArtItem(int objectID, String identificationNumber, String title, String artist, String material, String description, String surface, String dateOfPlacement, String img){
+        this.objectID = objectID;
+        this.identificationNumber = identificationNumber;
+        this.title = title;
+        this.artist = artist;
+        this.material = material;
+        this.description = description;
+        this.surface = surface;
+        this.dateOfPlacement = dateOfPlacement;
+        this.img = img;
+    }
+
+    public int getObjectID() {
+        return objectID;
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSurface() {
+        return surface;
+    }
+
+    public String getDateOfPlacement() {
+        return dateOfPlacement;
+    }
+
+    public String getImg() {
+        return img;
+    }
 }
--- Sa'ad Abdullah 2154528/app/src/main/java/com/example/android/sierendeelementen/NetworkUtils.java
+++ Sa'ad Abdullah 2154528/app/src/main/java/com/example/android/sierendeelementen/NetworkUtils.java
@@ -1,4 +1,59 @@
 package com.example.android.sierendeelementen;
 
-public class NetworkUtils {
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetworkUtils extends AsyncTask<String, Void, String> {
+    private TextView textView;
+
+    public NetworkUtils(TextView textView) {
+        this.textView = textView;
+
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String result = "";
+
+        try {
+            URL url = new URL(strings[0]);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream stream = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+            StringBuilder builder = new StringBuilder();
+
+            String inputString;
+            while ((inputString = bufferedReader.readLine()) != null) {
+                builder.append(inputString);
+            }
+
+            result = builder.toString();
+            JSONObject topLevel = new JSONObject(builder.toString());
+            //JSONObject main = topLevel.getJSONObject("products");
+            // result = String.valueOf(main.getDouble("title"));
+
+            urlConnection.disconnect();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String product) {
+        textView.setText("Current Product: " + product);
+    }
+
 }
